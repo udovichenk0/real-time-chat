c7cf6ffa2d2cd5244c7a2adc725beafdd5cbfd22
Denis <denzeldenisq@gmail.com>
2023-03-20 19:18:45 -0200

    change

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -4,6 +4,5 @@ const AuthController = require('../controllers/authController')
 router.get('/signin', AuthController.login)
 router.post('/signin', AuthController.signin)
 router.post('/signup', AuthController.signup)
-router.get('/test', (req,res) => res.send('asd'))
 
 module.exports = router
